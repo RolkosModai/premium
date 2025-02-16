dd12a6060996834a3d24d8653481210c0c346a4a
RolkosModai <74825391+RolkosModai@users.noreply.github.com>
2025-02-16 12:41:48 +0200

    Update limit.sh

--- files/limit.sh
+++ files/limit.sh
@@ -7,7 +7,7 @@
 # whatsapp   » wa.me/+254716637803
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # TOXIC TECH࿐
-REPO="https://raw.githubusercontent.com/savagetech56/premium/main/"
+REPO="https://raw.githubusercontent.com/RolkosModai/premium/main/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1

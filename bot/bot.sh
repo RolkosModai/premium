6cb6f24be2e6d18274c69658af6ff33d4ea13cf5
RolkosModai <74825391+RolkosModai@users.noreply.github.com>
2025-02-16 12:37:01 +0200

    Add files via upload

--- bot/bot.sh
+++ bot/bot.sh
@@ -18,13 +18,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/savagetech56/premium/main/bot/bot.zip
+wget https://raw.githubusercontent.com/RolkosModai/premium/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/savagetech56/premium/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/RolkosModai/premium/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
